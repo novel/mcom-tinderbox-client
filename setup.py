3c73a9451e686b6787df56815654a8659c1dae35
Roman Bogorodskiy <bogorodskiy@gmail.com>
2011-01-30 15:27:56 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="tbclient",
+        version="0.1",
+        description="Command line client for Marcuscom Tinderbox REST service",
+        author="Roman Bogorodskiy",
+        author_email="novel@FreeBSD.org",
+        packages=["tbclient.commands", "tbclient"],
+        scripts=["tbc.py"],
+)
